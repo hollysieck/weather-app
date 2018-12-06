e7063ea8ae4e4af6d3a1e34008147edd72e12b97
Holly Sieck <hollysieck@gmail.com>
2018-12-06 16:14:50 -0600

    Weather App Deployes Successfully to Heroku

--- weather-app/server.js
+++ weather-app/server.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {   // the slash means that we are exclusivley 
 })
 
 //creates a server
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
